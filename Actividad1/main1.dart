9f50f87eef306c33bc0a1d2f602f74c760626dd3
AlvarezAngel. <Miguelanproyt123@gmail.com>
2025-10-17 14:24:55 -0500

    Delete Actividad1 directory

--- Actividad1/main1.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  
-  double velocidad = 20.0;
-  double tiempo = 10.0;
-  double distancia = velocidad * tiempo;
-  
-  print('La distancia recorrida es: $distancia metros');
-}
--- Actividad1/main6.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  double celsius = 70.0;
-
-  double fahrenheit = (celsius * 1.8) + 32;
-
-  print('$celsius°C equivale a ${fahrenheit.toStringAsFixed(2)}°F');
-}
